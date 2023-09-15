2ce1a62914fd592570701e8233560f14b452911f
thanhtri1010 <trithanhh1010@gmail.com>
2023-09-15 13:57:28 +0700

    Initial commit

---
+++ src/main/java/com/example/SecurityJWT/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.example.SecurityJWT.repository;
+
+import java.util.Optional;
+
+import java.util.Optional;
+
+import com.example.SecurityJWT.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
